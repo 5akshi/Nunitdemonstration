f611491fc95eb84e40f5f94413f8da6a01b8ac06
5akshi <sakshii1230@gmail.com>
2024-02-12 19:15:17 -0500

    Add project files.

---
+++ Nunitdemonstration/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace Nunitdemonstration
+{
+    public class Class1
+    {
+        public static class Triangle
+        {
+            // PROG8041-Sakshi Desai- 8995937 -LAB 2
+            public static string ValidTriangle(int firstAngle, int secondAngle, int thirdAngle)
+            {
+                string result;
+                if ((firstAngle + secondAngle + thirdAngle) == 180)
+                {
+                    result = "The triangle is valid.";
+                }
+                else
+                {
+                    result = "The triangle is NOT valid.";
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ Testclass/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Legacy;
+using NUnit.Framework;
+using Nunitdemonstration;
+using static Nunitdemonstration.Class1;
+
+namespace Nunitdemonstration
+{
+    public class TriangleTest
+    {
+        // PROG8041-Sakshi Desai- 8995937 -LAB 2
+        [TestFixture]
+        public class TriangleTest2
+        {
+            [Test]
+            public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+            { 
+                //Arrange
+                int firstAngle = 60;
+                int secondAngle = 60;
+                int thirdAngle = 60;
+
+                string expected = "The triangle is valid.";
+                //Act 
+                string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+                //Assert
+                ClassicAssert.AreEqual(expected, actual);
+           }
+        }
+
+    }
+}
